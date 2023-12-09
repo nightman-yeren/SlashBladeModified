b42f29895096d380fce250b7e47aa5106056b439
Nightman <136943234+nightman-yeren@users.noreply.github.com>
2023-12-08 23:53:33 -0500

    Added slowness when hitting a player and the blade that is held has 1000 kill counts

--- src/main/java/mods/flammpfeil_yuruni/slashblade/util/AttackManager.java
+++ src/main/java/mods/flammpfeil_yuruni/slashblade/util/AttackManager.java
@@ -10,15 +10,17 @@ import mods.flammpfeil_yuruni.slashblade.entity.EntityAbstractSummonedSword;
 import mods.flammpfeil_yuruni.slashblade.entity.EntitySlashEffect;
 import mods.flammpfeil_yuruni.slashblade.entity.IShootable;
 import mods.flammpfeil_yuruni.slashblade.item.ItemSlashBlade;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
@@ -198,6 +200,10 @@ public class AttackManager {
                     try {
                         state.setOnClick(true);
                         attacker.getAttribute(Attributes.ATTACK_DAMAGE).addTransientModifier(am);
+                        int killCount = state.getKillCount();
+                        if (killCount >= 1000 && t instanceof Player) {
+                            ((Player) t).addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 10));
+                        }
 
                         ((Player) attacker).attack(t);
 
